bfc8135f7e9bce3f67f5e6e2430fb72f71bbfd94
Mankan <camaramacky73@gmail.com>
2024-08-10 17:32:42 +0000

    fin add et update infrastructure

--- src/SuiviInfrastructure/SuiviForm.js
+++ src/SuiviInfrastructure/SuiviForm.js
@@ -7,14 +7,19 @@ import {
   StyleSheet,
   Alert,
   TouchableOpacity,
+  Image
 } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
 import InfrastructureService from '../../services/infrastructure';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import { Button } from 'react-native-paper';
 import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { Audio, Video } from 'expo-av';
 
-const SuiviForm = ({ codeInfrastructure, closeModal, existingSuivi }) => {
+const SuiviForm = ({ codeInfrastructure, closeModal, existingSuivi, onSuiviAdded }) => {
+  const [loading, setLoading] = useState(false);
+
   const [datePickerDate, setDatePickerDate] = useState(
     existingSuivi ? new Date(existingSuivi.DateSuivi) : new Date()
   );
@@ -30,8 +35,11 @@ const SuiviForm = ({ codeInfrastructure, closeModal, existingSuivi }) => {
       : '',
     Difficultes: existingSuivi ? existingSuivi.Difficultes : '',
   });
-  const [images, setImages] = useState([]);
-  const [videos, setVideos] = useState([]);
+  const [images, setImages] = useState(existingSuivi ? existingSuivi.images || [] : []);
+const [videos, setVideos] = useState(existingSuivi ? existingSuivi.videos || [] : []);
+
+  // const [images, setImages] = useState([]);
+  // const [videos, setVideos] = useState([]);
   const [errors, setErrors] = useState({});
 
   const handleInputChange = (name, value) => {
@@ -40,7 +48,12 @@ const SuiviForm = ({ codeInfrastructure, closeModal, existingSuivi }) => {
 
   const handleMediaSelect = async (mediaType) => {
     let result;
+
     if (mediaType === 'image') {
+      if (images.length >= 3) {
+        Alert.alert('Limite atteinte', 'Vous ne pouvez ajouter que trois images.');
+        return;
+      }
       result = await ImagePicker.launchImageLibraryAsync({
         mediaTypes: ImagePicker.MediaTypeOptions.Images,
         allowsEditing: true,
@@ -102,16 +115,27 @@ const SuiviForm = ({ codeInfrastructure, closeModal, existingSuivi }) => {
     if (!suiviDetails.TauxAvancementTechnique)
       newErrors.TauxAvancementTechnique =
         'Taux Avancement Technique est obligatoire';
+    if (images.length === 0) {
+      newErrors.images = 'Au moins une image est requise';
+    }
     setErrors(newErrors);
 
     return Object.keys(newErrors).length === 0;
   };
 
+
+
+
   const handleSubmit = async () => {
+
+
     if (!validateForm()) {
       return;
     }
-
+    setLoading(true);
+    console.log('Détails du Suivi:', suiviDetails);
+    console.log('Images:', images);
+    console.log('Vidéos:', videos);
     try {
       let response;
       if (suiviDetails.id) {
@@ -131,6 +155,7 @@ const SuiviForm = ({ codeInfrastructure, closeModal, existingSuivi }) => {
       }
 
       if (response) {
+        console.log('Réponse de l\'API:', response);
         closeModal();
         Alert.alert(
           'Succès',
@@ -139,20 +164,30 @@ const SuiviForm = ({ codeInfrastructure, closeModal, existingSuivi }) => {
             : 'Suivi ajouté avec succès'
         );
       }
+      if (onSuiviAdded) {
+        onSuiviAdded(response);
+      }
     } catch (error) {
       console.error('Erreur lors de l\'envoi des données :', error);
       Alert.alert(
         'Erreur',
         'Une erreur est survenue lors de l\'envoi des données'
       );
+    } finally {
+      setLoading(false); // Arrêter l'indicateur de chargement
     }
+  };
+
+  const removeImage = (index) => {
+    setImages((prevImages) => prevImages.filter((_, i) => i !== index));
+  };
+
+  const removeVideo = (index) => {
+    setVideos((prevVideos) => prevVideos.filter((_, i) => i !== index));
   };
 
   return (
     <KeyboardAwareScrollView contentContainerStyle={styles.container}>
-      <Text style={{fontSize:20, fontWeight:'500',paddingBottom:10}}>
-        Ajouter un suivi
-      </Text>
       <View style={styles.inputContainer}>
         <Text style={styles.label}>Date de Suivi</Text>
         <TouchableOpacity onPress={handleDatePickerOpen}>
@@ -230,7 +265,10 @@ const SuiviForm = ({ codeInfrastructure, closeModal, existingSuivi }) => {
           multiline
         />
       </View>
-
+      <Text style={styles.label}>Images</Text>
+      {errors.images && (
+        <Text style={styles.error}>{errors.images}</Text>
+      )}
       <View style={styles.mediaButtonContainer}>
         <Button
           icon="camera"
@@ -247,10 +285,38 @@ const SuiviForm = ({ codeInfrastructure, closeModal, existingSuivi }) => {
           Ajouter une Vidéo
         </Button>
       </View>
+      <View style={styles.mediaPreviewContainer}>
+      {images.map((image, index) => (
+  <View key={index} style={styles.mediaThumbnailContainer}>
+    <TouchableOpacity onPress={() => removeImage(index)} style={styles.removeIcon}>
+      <Icon name="cancel" size={24} color="#fff" />
+    </TouchableOpacity>
+    <Image source={{ uri: image.uri }} style={styles.mediaThumbnail} />
+  </View>
+))}
+{videos.map((video, index) => (
+  <View key={index} style={styles.mediaThumbnailContainer}>
+    <TouchableOpacity onPress={() => removeVideo(index)} style={styles.removeIcon}>
+      <Icon name="cancel" size={24} color="#fff" />
+    </TouchableOpacity>
+    <Video
+      source={{ uri: video.uri }}
+      style={styles.mediaThumbnail}
+      useNativeControls
+      resizeMode="cover"
+    />
+  </View>
+))}
 
-      <Button mode="contained" onPress={handleSubmit}>
-        {suiviDetails.id ? 'Mettre à jour le Suivi' : 'Ajouter le Suivi'}
+      </View>
+      <Button
+        mode="contained"
+        onPress={handleSubmit}
+        loading={loading}
+      >
+        {loading ? 'Chargement...' : (suiviDetails.id ? 'Mettre à jour le Suivi' : 'Ajouter le Suivi')}
       </Button>
+
     </KeyboardAwareScrollView>
   );
 };
@@ -286,6 +352,38 @@ const styles = StyleSheet.create({
     justifyContent: 'space-around',
     marginBottom: 16,
   },
+  mediaPreviewContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+    marginBottom: 16,
+  },
+  mediaThumbnail: {
+    width: 80,
+    height: 80,
+    margin: 4,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 4,
+    overflow: 'hidden',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  videoText: {
+    color: '#000',
+    fontWeight: 'bold',
+  },
+  removeIcon: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    backgroundColor: 'red',
+    zIndex: 1,
+    borderRadius: 12,
+    padding: 6,
+    elevation: 4,
+  }
+
 });
 
 export default SuiviForm;
--- src/SuiviInfrastructure/SuiviInfrastructure.js
+++ src/SuiviInfrastructure/SuiviInfrastructure.js
@@ -7,6 +7,7 @@ import {
   Modal,
   StyleSheet,
   TouchableOpacity,
+  Dimensions,
 } from 'react-native';
 import {
   ProgressBar,
@@ -14,6 +15,7 @@ import {
   FAB,
   IconButton,
 } from 'react-native-paper';
+import { Video } from 'expo-video';
 import AuthService from '../../services/infrastructure';
 import { useTheme } from '../SettingsPage/themeContext';
 import SuiviForm from './SuiviForm';
@@ -25,8 +27,12 @@ const SuiviInfrastructure = ({ route }) => {
   const [loading, setLoading] = useState(true);
   const [modalVisible, setModalVisible] = useState(false);
   const [selectedSuivi, setSelectedSuivi] = useState(null);
-  const [fullscreenImage, setFullscreenImage] = useState(null);
+  const [fullscreenMedia, setFullscreenMedia] = useState(null);
+  const [suivis, setSuivis] = useState([]);
 
+  const updateSuivisList = (newSuivi) => {
+    setSuivis(prevSuivis => [newSuivi, ...prevSuivis]);
+  };
   useEffect(() => {
     const fetchDetails = async () => {
       try {
@@ -54,14 +60,17 @@ const SuiviInfrastructure = ({ route }) => {
   const hideModal = () => {
     setModalVisible(false);
     setSelectedSuivi(null);
+    updateSuivisList(selectedSuivi);
   };
 
-  const openFullscreenImage = (imageUri) => {
-    setFullscreenImage(imageUri);
+  const openFullscreenMedia = (mediaUri) => {
+    if (mediaUri) {
+      setFullscreenMedia(mediaUri);
+    }
   };
 
-  const closeFullscreenImage = () => {
-    setFullscreenImage(null);
+  const closeFullscreenMedia = () => {
+    setFullscreenMedia(null);
   };
 
   if (loading) {
@@ -76,6 +85,27 @@ const SuiviInfrastructure = ({ route }) => {
     );
   }
 
+  const renderImages = (photos) => {
+    const imageUris = photos.split('|');
+    if (imageUris.length === 1) {
+      return (
+        <TouchableOpacity onPress={() => openFullscreenMedia(imageUris[0])}>
+          <Image source={{ uri: imageUris[0] }} style={styles.fullImage} />
+        </TouchableOpacity>
+      );
+    } else {
+      return (
+        <View style={styles.gridContainer}>
+          {imageUris.map((photoUri, idx) => (
+            <TouchableOpacity key={idx} onPress={() => openFullscreenMedia(photoUri)}>
+              <Image source={{ uri: photoUri }} style={styles.gridImage} />
+            </TouchableOpacity>
+          ))}
+        </View>
+      );
+    }
+  };
+
   return (
     <View style={[styles.container, { backgroundColor: theme.colors.background }]}>
       <ScrollView>
@@ -87,14 +117,15 @@ const SuiviInfrastructure = ({ route }) => {
           {infrastructure.suivis &&
             infrastructure.suivis.map((suivi, index) => (
               <View key={index} style={[styles.detailCard, { backgroundColor: theme.colors.card }]}>
-                <TouchableOpacity onPress={() => openFullscreenImage(suivi.Photos)}>
-                  <Image source={{ uri: suivi.Photos }} style={styles.image} />
-                </TouchableOpacity>
+                {/* Handle multiple images */}
+                {suivi.Photos && renderImages(suivi.Photos)}
                 {suivi.videos && (
-                  <TouchableOpacity>
-                    <Image
+                  <TouchableOpacity onPress={() => openFullscreenMedia(suivi.videos)}>
+                    <Video
                       source={{ uri: suivi.videos }}
-                      style={styles.image}
+                      style={styles.video}
+                      resizeMode="contain"
+                      useNativeControls
                     />
                   </TouchableOpacity>
                 )}
@@ -102,7 +133,7 @@ const SuiviInfrastructure = ({ route }) => {
                   progress={parseFloat(suivi.TauxAvancementTechnique) / 100}
                   color={theme.primary}
                 />
-                <View style={{flexDirection:'row' ,justifyContent:'space-between'}}>
+                <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
                   <View>
                     <Text
                       style={{ color: theme.colors.text }}
@@ -112,7 +143,7 @@ const SuiviInfrastructure = ({ route }) => {
                     >{`Montant Décaisser: ${suivi.MontantDecaisser}`}</Text>
                     <Text style={{ color: theme.colors.text }}>{`Difficulté: ${suivi.Difficultes}`}</Text>
                   </View>
-                  <View style={{paddingTop:10}}>
+                  <View style={{ paddingTop: 10 }}>
                     <IconButton
                       icon="pencil"
                       size={24}
@@ -133,6 +164,9 @@ const SuiviInfrastructure = ({ route }) => {
       />
       <Modal visible={modalVisible} onRequestClose={hideModal}>
         <View style={styles.modalContainer}>
+          <Text style={{ fontSize: 20, fontWeight: '500', paddingBottom: 10 }}>
+            Ajouter un suivi
+          </Text>
           <IconButton
             icon="close"
             size={30}
@@ -144,16 +178,26 @@ const SuiviInfrastructure = ({ route }) => {
             codeInfrastructure={infrastructure.CodeInfrastructure}
             closeModal={hideModal}
             existingSuivi={selectedSuivi}
+            onSuiviAdded={updateSuivisList}
           />
         </View>
       </Modal>
-      {/* Fullscreen Image Modal */}
-      <Modal visible={!!fullscreenImage} transparent={true} onRequestClose={closeFullscreenImage}>
+      {/* Fullscreen Media Modal */}
+      <Modal visible={!!fullscreenMedia} transparent={true} onRequestClose={closeFullscreenMedia}>
         <View style={styles.fullscreenContainer}>
-          <TouchableOpacity onPress={closeFullscreenImage} style={styles.fullscreenCloseButton}>
+          <TouchableOpacity onPress={closeFullscreenMedia} style={styles.fullscreenCloseButton}>
             <Text style={styles.fullscreenCloseText}>fermer</Text>
           </TouchableOpacity>
-          <Image source={{ uri: fullscreenImage }} style={styles.fullscreenImage} />
+          {fullscreenMedia && fullscreenMedia.endsWith('.mp4') ? (
+            <Video
+              source={{ uri: fullscreenMedia }}
+              style={styles.fullscreenImage}
+              resizeMode="contain"
+              useNativeControls
+            />
+          ) : (
+            <Image source={{ uri: fullscreenMedia }} style={styles.fullscreenImage} />
+          )}
         </View>
       </Modal>
     </View>
@@ -161,7 +205,7 @@ const SuiviInfrastructure = ({ route }) => {
 };
 
 const styles = StyleSheet.create({
-  container:{
+  container: {
     flex: 1,
   },
   fab: {
@@ -172,9 +216,9 @@ const styles = StyleSheet.create({
   },
   modalContainer: {
     flex: 1,
+    paddingTop: 50,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'rgba(0,0,0,0.5)',
   },
   card: {
     padding: 10,
@@ -192,10 +236,32 @@ const styles = StyleSheet.create({
     resizeMode: 'contain',
     marginVertical: 10,
   },
+  fullImage: {
+    width: '100%',
+    height: 300,
+    resizeMode: 'contain',
+  },
+  gridContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+  },
+  gridImage: {
+    width: Dimensions.get('window').width / 2 - 20, // Two images per row
+    height: 150,
+    marginBottom: 10,
+    borderRadius: 5,
+  },
+  video: {
+    width: '100%',
+    height: 200,
+    marginVertical: 10,
+  },
   closeButton: {
     position: 'absolute',
     top: 10,
     right: 10,
+    zIndex: 1, // Assurez-vous que le bouton est au-dessus des autres éléments
   },
   textBold: {
     fontSize: 18,
